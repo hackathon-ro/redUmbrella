afc46524b0c53e85af837171200848230dd81eec
Marius-Constantin Postolache <marius@postolache.net>
2013-04-20 18:05:25 +0300

    changes in design

--- webui/protected/views/layouts/main.php
+++ webui/protected/views/layouts/main.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hackathon - 2013</title>
+    <title>SmartTime</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->getBaseUrl()?>/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->getBaseUrl()?>/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->getBaseUrl()?>/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->getBaseUrl()?>/css/style.css" rel="stylesheet">
   </head>
   <body>
       <?php echo $content?> 
 
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?php echo Yii::app()->getBaseUrl()?>js/bootstrap.min.js"></script>
   </body>
 </html>
