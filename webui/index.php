0d21ec02d109e0fb4871c225d2d323d874f50c0d
Marius-Constantin Postolache <marius@postolache.net>
2013-04-20 22:31:55 +0300

    dynamic index.php

--- webui/index.php
+++ webui/index.php
@@ -1,13 +1,21 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+switch ($_SERVER['SERVER_NAME']) {
+    case '10.10.0.20':
+        $yii = dirname(__FILE__) . '/../yii/framework/yii.php';
+        break;
+    default:
+        $yii = dirname(__FILE__) . '/../../yii/framework/yii.php';
+        break;
+}
+
+$config = dirname(__FILE__) . '/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
