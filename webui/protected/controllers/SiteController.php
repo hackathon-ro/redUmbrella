6c545cd3197412e6eec12568964694669ec162e3
unknown <anadyon_dz@yahoo.com>
2013-04-20 23:23:02 +0300

    errors handling

--- webui/protected/controllers/SiteController.php
+++ webui/protected/controllers/SiteController.php
@@ -46,4 +46,15 @@ class SiteController extends Controller
         die;
         $this->render('getValues', array(''));
     }
+    
+    public function actionError()
+    {
+        if($error=Yii::app()->errorHandler->error)
+        {
+            if(Yii::app()->request->isAjaxRequest)
+                    echo $error['message'];
+            else
+                    $this->render('error', $error);
+        }
+    }
 }
