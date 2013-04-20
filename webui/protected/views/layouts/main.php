aba9922a3bdb415e25717253b5922ddee97f0398
Marius-Constantin Postolache <marius@postolache.net>
2013-04-20 14:15:26 +0300

    initial source

---
+++ webui/protected/views/layouts/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hackathon - 2013</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+      <div class="container">
+      <?php echo $content?>
+      </div>    
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
