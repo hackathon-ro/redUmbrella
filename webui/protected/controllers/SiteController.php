252e93f3f04855c761d4b74860fd5fd02ecbf406
Marius-Constantin Postolache <marius@postolache.net>
2013-04-21 00:15:01 +0300

    generate order

--- webui/protected/controllers/SiteController.php
+++ webui/protected/controllers/SiteController.php
@@ -1,64 +1,61 @@
 <?php
 
-class SiteController extends Controller
-{
+class SiteController extends Controller {
+
     /**
      * This is the default 'index' action that is invoked
      * when an action is not explicitly requested by users.
      */
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $allCategories = ProductCategory::model()->findAll();
-        
-        $this->render('index', array('allCategories'=>$allCategories));
+
+        $this->render('index', array('allCategories' => $allCategories));
     }
-    
-    public function actionGetValues()
-    {
+
+    public function actionGetValues() {
         $arrProducts = array();
-        
+
         $max = 1024;
         $file = file_get_contents('/var/scripts/sensor_readings.txt');
         $getValues = explode(' ', $file);
-        
-        foreach ( $getValues as $key => $value ) { /**key => sensor_channel_no from product table**/
-            /**get notification_level from table**/
-            $notificationLevel = Product::model()->find( 'sensor_channel_no =:sensonChannelNo', array(':sensonChannelNo' => $key) );
-            if ( $notificationLevel ) {
-                $makePercentage = round( ($value*100)/$max ); /**get percentage from file**/
-                if ( $makePercentage <= $notificationLevel->notification_level ) { /**compare values**/
+
+        foreach ($getValues as $key => $value) { /*         * key => sensor_channel_no from product table* */
+            /*             * get notification_level from table* */
+            $notificationLevel = Product::model()->find('sensor_channel_no =:sensonChannelNo', array(':sensonChannelNo' => $key));
+            if ($notificationLevel) {
+                $makePercentage = round(($value * 100) / $max); /*                 * get percentage from file* */
+                if ($makePercentage <= $notificationLevel->notification_level) { /*                 * compare values* */
                     //echo '('.$key.')'.$notificationLevel->name.' must be refilled(Level from table:'.$notificationLevel->notification_level.'; Level from file: '.$makePercentage.')<br />';
                     $arrProducts[] = $notificationLevel->name;
                 }
             }
         }
-        if ( !empty( $arrProducts ) ) {
+        if (!empty($arrProducts)) {
             print_r($arrProducts);
-            $text = 'Plese order the following products '.  implode(" ", $arrProducts);
-            $phone = Settings::model()->find( '`key` = "phone"' );
-            
-            $file = dirname(__FILE__).'/../runtime/tts.txt';
+            $text = 'Plese order the following products ' . implode(" ", $arrProducts);
+            $phone = Settings::model()->find('`key` = "phone"');
+
+            $file = dirname(__FILE__) . '/../runtime/tts.txt';
             file_put_contents($file, $text);
-            $command1 = ' cat '.$file.' | text2wave -o /usr/share/asterisk/sounds/tts.ulaw -otype ulaw';
+
+            $command1 = ' cat ' . $file . ' | text2wave -o /usr/share/asterisk/sounds/tts.ulaw -otype ulaw';
             exec($command1, $output, $err);
-            
-            $command2 = 'asterisk -rx "originate sip/'.$phone->value.' extension 10@tts-message"';
-            echo $command2;
+
+            $command2 = '/usr/sbin/asterisk -rx "originate sip/' . $phone->value . ' extension 10@tts-message"';
             exec($command2, $output, $err);
         }
-        
+
         die;
         $this->render('getValues', array(''));
     }
-    
-    public function actionError()
-    {
-        if($error=Yii::app()->errorHandler->error)
-        {
-            if(Yii::app()->request->isAjaxRequest)
-                    echo $error['message'];
+
+    public function actionError() {
+        if ($error = Yii::app()->errorHandler->error) {
+            if (Yii::app()->request->isAjaxRequest)
+                echo $error['message'];
             else
-                    $this->render('error', $error);
+                $this->render('error', $error);
         }
     }
+
 }
